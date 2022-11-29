54502e655914da6ae18bc8a6712b1fd4d12d18f4
vincent385 <vincentbell103@outlook.com>
2022-11-29 08:04:24 +0000

    removed 2 files

--- buttons.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from tkinter import PhotoImage, Button
-
-
-class DefaultButton(Button):
-    def __init__(self, master, image, x, y, width, height, command):
-        super().__init__(master, image=image, borderwidth=0, highlightthickness=0, command=command,
-                         relief="flat")
-        self.place(x=x, y=y, width=width, height=height)
